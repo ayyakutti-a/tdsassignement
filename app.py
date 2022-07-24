4d93b5a1e621e2106171d0d0bdfd1dccbca3d065
ayyakutti-a <109910611+ayyakutti-a@users.noreply.github.com>
2022-07-24 22:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title ("IITM BSC Data Science - SUM APP")
+st.title ("IITM BSC Data Science - Multiplication APP")
 st.subheader (" Welcome to Multiplication of 2 given numbers APP - You can Multiply Two Numbers")
 
 a = st.number_input('Enter the First number')
